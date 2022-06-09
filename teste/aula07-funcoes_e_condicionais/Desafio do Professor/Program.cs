0e3ca12803742b320902c4fb4eaafb67d5a0ada8
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-09 16:44:31 -0300

    Desafio - Calcula o IMC

--- teste/aula07-funcoes_e_condicionais/Desafio do Professor/Program.cs
+++ teste/aula07-funcoes_e_condicionais/Desafio do Professor/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿/*
+Resultado do cálculo do IMC
+Abaixo de 18.5
+Entre 18.5 e 24.9
+Entre 25 e 29.9
+Acima de 30
+Maior que 40
+Diagnóstico
+Anêmico
+Normal
+Sobrepeso
+Obesidade
+Obesidade grave
+*/
+
+double IMC, peso, altura;
+
+Console.WriteLine("Informe o seu peso: ");
+peso = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe a sua altura: ");
+altura = double.Parse(Console.ReadLine());
+
+
+
+string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario) {
+    IMC = pesoDoUsuario / (alturaDoUsuario* alturaDoUsuario);
+}
+
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Seu diagnóstico é XXXXXX.
+Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(peso, altura)}");
+
