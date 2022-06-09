4ca16c80abfd50387e2f95b0635362453f3b69c0
IasmimVivanco <iasmim.vivanco@hotmail.com>
2022-06-09 17:35:55 -0300

    Resolvemos o desafio 2

--- teste/aula07-funcoes_e_condicionais/Desafio do Professor/Program.cs
+++ teste/aula07-funcoes_e_condicionais/Desafio do Professor/Program.cs
@@ -1,32 +1,41 @@
-﻿/*
-Resultado do cálculo do IMC
-Abaixo de 18.5
-Entre 18.5 e 24.9
-Entre 25 e 29.9
-Acima de 30
-Maior que 40
-Diagnóstico
-Anêmico
-Normal
-Sobrepeso
-Obesidade
-Obesidade grave
-*/
+﻿using System;
+
+using System.Globalization;
 
 double IMC, peso, altura;
 
-Console.WriteLine("Informe o seu peso: ");
-peso = double.Parse(Console.ReadLine());
-Console.WriteLine("Informe a sua altura: ");
-altura = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe o seu peso em kilos: ");
+peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Informe a sua altura em metros: ");
+altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 
 
 string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario) {
-    IMC = pesoDoUsuario / (alturaDoUsuario* alturaDoUsuario);
+IMC = pesoDoUsuario / (alturaDoUsuario * alturaDoUsuario);
+    if (IMC < 18.5)
+    {
+        return "Abaixo do peso.";
+    }
+    else if (IMC <= 24.9)
+    {
+        return "Peso Normal.";
+    }
+    else if (IMC <= 30)
+    {
+        return "Sobrepeso";
+    }
+    else if(IMC <= 40)
+    {
+        return "Obesidade";
+    }
+    else
+    {
+        return "Obesidade grave";
+    }
 }
 
 // Utilize a mensagem a seguir como base do retorno esperado:
 // Seu diagnóstico é XXXXXX.
-Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(peso, altura)}");
+Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(altura,peso)}");
 
